6f5d32b597797a2c8e4c39d46a109b27be430ed7
suches <qiushaowei@360.cn>
2016-07-21 18:31:24 +0800

    up

---
+++ mv.videoevent.js
@@ -0,0 +1,27 @@
+
+//http://www.w3schools.com/tags/ref_av_dom.asp
+
+module.exports = {
+    abort: 'abort'
+    , canplay: 'canplay'
+    , canplaythrough: 'canplaythrough'
+    , durationchange: 'durationchange'
+    , emptied: 'emptied'
+    , ended: 'ended'
+    , error: 'error'
+    , loadeddata: 'loadeddata'
+    , loadedmetadata: 'loadedmetadata'
+    , loadstart: 'loadstart'
+    , pause: 'pause'
+    , play: 'play'
+    , playing: 'playing'
+    , progress: 'progress'
+    , ratechange: 'ratechange'
+    , seeked: 'seeked'
+    , seeking: 'seeking'
+    , stalled: 'stalled'
+    , suspend: 'suspend'
+    , timeupdate: 'timeupdate'
+    , volumechange: 'volumechange'
+    , waiting: 'waiting'
+}
